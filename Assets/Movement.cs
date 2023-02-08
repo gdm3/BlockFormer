2ca7f3c93374493d6c88c6a0d53d958d319e0ee2
gdm3 <64926297+gdm3@users.noreply.github.com>
2023-02-07 19:24:40 -0500

    added features

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,5 +1,6 @@
 
 
+using System.Collections.ObjectModel;
 using System.Numerics;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,8 +9,9 @@ using Vector2 = UnityEngine.Vector2;
 public class Movement : MonoBehaviour
 {
 
-
+    public int jumps = 0;
     public Rigidbody2D rb;
+    private bool isCollidingWithWall = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,17 +23,49 @@ public class Movement : MonoBehaviour
     {
          if (Input.GetKeyDown("space"))
         {
-            rb.velocity = new Vector2(rb.velocity.x, 5);
+            if (jumps < 2)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, 5);
+                jumps++;
+            }
+            if(isCollidingWithWall == true)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, 5);
+                jumps = 1;
+            }
+
         }
         if(Input.GetKey("d"))
         {
-            rb.velocity += new Vector2(.25f, 0);
+            rb.velocity += new Vector2(.15f, 0);
         }
         if (Input.GetKey("a"))
         {
-            rb.velocity += new Vector2(-.25f, 0);
+            rb.velocity += new Vector2(-.15f, 0);
         }
         rb.velocity = new Vector2(rb.velocity.x * 0.965f, rb.velocity.y);
-
+    
     }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Lava"){
+            Debug.Log("Lava");
+            gameObject.transform.position = new Vector2(-6, -2.7f);
+        }
+        if (col.gameObject.tag == "Sides")
+        {
+            Debug.Log("Wall");
+            isCollidingWithWall = true;
+        }
+        jumps = 0;
+    }
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Sides")
+        {
+            Debug.Log("Wall");
+            isCollidingWithWall = false;
+        }
+    }
+
 }
